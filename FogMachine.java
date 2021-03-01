01d6568d84a15471e5b68594bc4adedec29dc2ce
Eric Darsow <edarsow@ccac.edu>
2021-03-01 10:36:40 -0500

    Sample upload of .java file

---
+++ FogMachine.java
@@ -0,0 +1,31 @@
+
+package components.comp3_objects.project;
+
+/**
+ * A skeleton class that models the functionality of a Fog machine
+ * @author Technology Rediscovery
+ */
+public class FogMachine {
+    
+    final int MAX_FLUID_ML = 400;
+    private int currentFluidLevel = 0;
+    // more member variables here
+    
+    // NO MAIN method in the "blueprint" class
+    
+    public double getFluidLevelPercent(){
+        
+        // write the guts of this method which calculates
+        // the percent of the fluid remaining in the machine
+        // by dividing the current level by the max level
+        
+        // return a double!
+        return 0.0;
+    } // close method
+    
+    // MORE METHODS HERE!
+    
+    // perhaps something like sprayFog that takes in an integer 
+    // for time to spray
+    
+} // close class
